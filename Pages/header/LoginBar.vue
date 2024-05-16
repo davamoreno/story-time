dae70a443a4d4141c7d6ea5173b9d2fdfe726090
Dava Moreno <davamoreno28@gmail.com>
2024-05-16 15:06:47 +0800

    buat login bar

---
+++ Pages/header/LoginBar.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <a type="button" class="btn btn-primary">Login</a>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+     @use "~/assets/styles/main.scss"
+</style>
